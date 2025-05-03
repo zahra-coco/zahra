935103726ab35e8ee71f0980acd17497dfb3ec9c
Zahra Putri Asifa <zp90866@gmail.com>
2025-05-02 22:41:58 -0700

    Update file login.php

--- login.php
+++ login.php
@@ -44,7 +44,7 @@ include "inc/koneksi.php";
 		<!-- /.login-logo -->
 		<div class="login-box-body">
 			<center>
-				<img src="logosmp" width=160px />
+				<img src="dish/img/logosmp.png" width=160px />
 			</center>
 			<br>
 			<p class="login-box-msg">Login System</p>
